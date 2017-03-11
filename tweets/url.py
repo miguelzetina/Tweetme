93f9d81ec2374867d8e2d887f51ef60fe3df79ea
Miguel Zetina <Loefthur26@gmail.com>
2017-03-11 13:24:21 -0600

    Entendiendo las vistas basadas en clases

--- tweets/url.py
+++ tweets/url.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 
-from .views import tweet_detail_view, tweet_list_view
+from .views import TweetDetailView, TweetListView
+# from .views import tweet_detail_view, tweet_list_view
 
 urlpatterns = [
-    url(r'^$', tweet_list_view, name="list"),
-    url(r'^1/$', tweet_detail_view, name="detail"),
+    # url(r'^$', tweet_list_view, name="list"),
+    # url(r'^1/$', tweet_detail_view, name="detail"),
+    url(r'^$', TweetListView.as_view(), name="list"),
+    url(r'^1/$', TweetDetailView.as_view(), name="detail"),
 ]
