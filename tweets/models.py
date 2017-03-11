3af10b5f0e9999697046d4b523ca9a10dfe9e8d6
Miguel Zetina <Loefthur26@gmail.com>
2017-03-10 20:22:20 -0600

    Comenzado el CRUD para los Tweets

---
+++ tweets/models.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+
+
+class Tweet(models.Model):
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL)
+    text        = models.TextField(max_length=140)
+    updated     = models.DateTimeField(auto_now=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.text)
---
+++ tweets/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from .models import Tweet
+# Create your views here.
+
+def tweet_detail_view(request, id=1):
+    obj = Tweet.objects.get(id=id)
+    context = {
+        "object": obj
+    }
+    return render(request, "tweets/detail_view.html", context)
+
+
+def tweet_list_view(request):
+    queryset = Tweet.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "tweets/list_view.html", context)
