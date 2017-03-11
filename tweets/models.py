d0eab69227b90012b46f9ba3c328510c6a292710
Miguel Zetina <Loefthur26@gmail.com>
2017-03-11 13:23:48 -0600

    Cambiado text=content

--- tweets/models.py
+++ tweets/models.py
@@ -5,10 +5,10 @@ from django.db import models
 
 
 class Tweet(models.Model):
-    user        = models.ForeignKey(settings.AUTH_USER_MODEL)
-    text        = models.TextField(max_length=140)
-    updated     = models.DateTimeField(auto_now=True)
-    timestamp   = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    content = models.TextField(max_length=140)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.text)
+        return str(self.content)
