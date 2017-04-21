0bb4c37c574e68718ffa2c8080a414072248df98
Miguel Zetina <Loefthur26@gmail.com>
2017-04-20 23:26:39 -0500

    Solución problema & HashTags: Problema: Tweet a mostar. Hashtag: Usando RegEx se añade el link para cada Hashtag en los tweets a mostrar (tweet_list.html)

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -25,7 +25,7 @@ class TweetListAPIView(generics.ListAPIView):
         im_following = self.request.user.profile.get_following()
         qs1 = Tweet.objects.filter(user__in=im_following)
         qs2 = Tweet.objects.filter(user=self.request.user)
-        qs = (qs1 | qs2).distint().order_by("-timestamp")
+        qs = (qs1 | qs2).distinct().order_by("-timestamp")
         # print(self.request.GET)
         query = self.request.GET.get("q", None)
         if query is not None:
